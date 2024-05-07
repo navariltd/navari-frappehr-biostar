41383b99958ca1ce2e813e3d7e7337c59b34ffab
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-05-07 09:46:26 +0300

    Biostar Settings DocType

---
+++ navari_biostar_integration/navari_biostar_integration/doctype/biostar_settings/biostar_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, navariltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Biostar Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ navari_biostar_integration/navari_biostar_integration/doctype/biostar_settings/test_biostar_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, navariltd and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBiostarSettings(FrappeTestCase):
+	pass
