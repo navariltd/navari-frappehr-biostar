dd1dcc2464c1e6c54508a6cd59c8ec9317936b46
emmanuel-mwendwa <mwendwae054@gmail.com>
2024-05-09 15:52:00 +0300

    Testing update

--- README.md
+++ README.md
@@ -1,6 +1,27 @@
 ## Navari Frappehr Biostar
 
-Frappe HR integration with Biostar Biometric
+Frappe HR integration with Biostar Biometrics
+
+#### DocTypes
+Biostar Settings
+
+In order to use the Biostar Settings app we have to login and acquire a cookie session id
+
+Generate the API key and API secret and feed them in the TA Auth Details section.
+
+1. Username: The user id used to log in to this page
+2. Password: The password used to log in to this page
+3. TA URL: The time and attendance URL
+4. API Key: API key generated for the user
+5. API Secret: API Secret generated for the user
+
+
+#### Summary
+Suprema Biostar is a web-based security platform that manages biometric access control systems and time attendance solutions.
+
+##### Key features
+1. Biometric Integration: Fingerprint, face recognition and card-based access for versatile and secure access control options.
+2. Attendance Record Fetch: Fetch attendance records from the system
 
 #### License
 
--- navari_frappehr_biostar/navari_frappehr_biostar/doctype/biostar_settings/test_biostar_settings.py
+++ navari_frappehr_biostar/navari_frappehr_biostar/doctype/biostar_settings/test_biostar_settings.py
@@ -9,20 +9,21 @@ from frappe.tests.utils import FrappeTestCase
 class TestBiostarSettings(FrappeTestCase):
     def setUp(self):
         """Initialize Frappe test environment"""
-        frappe.init(site="", sites_path="")
+        frappe.init(site="biostar.com", sites_path="/home/manu/frappe-bench/sites")
+        frappe.connect()
+        frappe.set_user("Administrator")
 
     def test_biostar_settings_creation(self):
         """Create a new Biostar Settings instance"""
         biostar_settings = frappe.get_doc(
             {
                 "doctype": "Biostar Settings",
-                "username": "test_user",
+                "username": "administrator",
                 "password": "test_password",
             }
         )
 
         biostar_settings.insert()
-
         self.assertIsNotNone(biostar_settings.name)
 
     def test_biostar_settings_validation(self):
@@ -37,6 +38,8 @@ class TestBiostarSettings(FrappeTestCase):
             biostar_settings.insert()
 
     def tearDown(self):
-        # Clean up after each test
-        frappe.db.rollback()
-        frappe.destroy()
+        """Clean up after each test"""
+        try:
+            frappe.db.rollback()
+        finally:
+            frappe.destroy()
